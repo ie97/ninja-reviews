3628a68d74a7ab9bca0ba8395e2d17759efdd2e2
ie97 <iwan.ie.id@gmail.com>
2021-08-22 12:07:38 +0700

    graphql homepage bloglist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// page & layout imports
 import SiteHeader from "./components/SiteHeader";
 import Category from "./pages/Category";
 import Homepage from "./pages/Homepage";
 import ReviewDetails from "./pages/ReviewDetails";
 
+// apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <SiteHeader />
-          <Switch>
-            <Route exact path="/">
-              <Homepage />
-            </Route>
-            <Route path="/details/:id">
-              <ReviewDetails />
-            </Route>
-            <Route path="/category/:id">
-              <Category />
-            </Route>
-          </Switch>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <SiteHeader />
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route path="/details/:id">
+                <ReviewDetails />
+              </Route>
+              <Route path="/category/:id">
+                <Category />
+              </Route>
+            </Switch>
+        </div>
+      </ApolloProvider>
     </Router>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,10 +7,24 @@
 // }
 
 import { Link } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
+// import useFetch from "../hooks/useFetch";
+import { useQuery, gql } from "@apollo/client";
+
+const REVIEWS = gql`
+  query GetReviews {
+    reviews{
+      title,
+      body,
+      rating,
+      id
+    }
+  }
+`
 
 const Homepage = () => {
-  const { loading, error, data } = useFetch('http://localhost:1337/reviews');
+  // const { loading, error, data } = useFetch('http://localhost:1337/reviews');
+
+  const { loading, error, data } = useQuery(REVIEWS)
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
@@ -19,7 +33,7 @@ const Homepage = () => {
 
   return (  
     <div>
-      {data.map(review => (
+      {data.reviews.map(review => (
         <div key={ review.id } className="review-card">
           <div className="rating">{ review.rating }</div>
           <h2>{ review.title }</h2>
